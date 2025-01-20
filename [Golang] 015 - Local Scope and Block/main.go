97e8fafe9567808823a5a6a028da10f268fe8335
somel's mac mini <somelsmac-mini@somelss-Mac-mini.local>
2025-01-20 09:32:10 +0600

    local scope added

---
+++ [Golang] 015 - Local Scope and Block/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var a = 20
+var b = 30
+
+func main(){
+	x:=40
+
+	if x > 20 {
+		p := 10
+		fmt.Println("I am matured boy - ", p, x)
+	}
+	fmt.Println("I am matured boy - ", p, x)
+}
